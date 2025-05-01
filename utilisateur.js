83114f2d42038f59a66799cd492972a4dad69875
EnzoHOU10 <129770379+EnzoHOU10@users.noreply.github.com>
2025-05-01 21:36:05 +0200

    Delete utilisateur.js

--- utilisateur.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/usercont');
-const auth = require('../middleware/authmid');
-
-router.put('/', auth, userController.updateUser); // Modification d'un utilisateur
-
-module.exports = router;
